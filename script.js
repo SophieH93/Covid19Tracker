eb98284a15b76b2fdb50e6e9ca5e3a8a847cfde2
Sophie <sophiehickey123@gmail.com>
2021-02-18 21:42:28 +0000

    add function to get covid data

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 
+window.onload = () => {
+    getCountryData();
+}
 
 let map;
 function initMap() {
@@ -7,4 +10,14 @@ function initMap() {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8,
     });
+}
+
+const getCountryData = () => {
+    // Fetch data from Covid API
+    fetch("https://disease.sh/v3/covid-19/countries")
+    .then((response)=>{
+        return response.json()
+    }).then((data)=>{
+        console.log(data);
+    })
 }
